a1172c52b5929c56fd0ed19cb3b5ad5f99635b7e
YvesND <yvdie@ulaval.ca>
2024-03-31 19:09:47 -0400

    1

---
+++ db_scripts/migrate_1.sql
@@ -0,0 +1,12 @@
+
+ALTER TABLE singer RENAME TO musician;
+ALTER TABLE musician ADD role varchar(50);
+ALTER TABLE musician ADD bandName varchar(50);
+UPDATE musician SET role = 'vocals', bandName = 'Crazy Duo' WHERE singerName = 'Alina';
+UPDATE musician SET role = 'guitar', bandName = 'Mysterio' WHERE singerName = 'Mysterio';
+UPDATE musician SET role = 'percussion', bandName = 'Crazy Duo' WHERE singerName = 'Rainbow';
+UPDATE musician SET role = 'piano', bandName = 'Luna' WHERE singerName = 'Luna';
+
+CREATE Table  band (bandName varchar(50), creation YEAR, genre VARCHAR(50), PRIMARY key (bandName));
+
+insert into band VALUES('Crazy Duo', 2015, 'rock'), ('Luna', 2009, 'classical'), ('Mysterio', 2019, 'pop');
---
+++ db_scripts/rollback_1.sql
@@ -0,0 +1,7 @@
+ALTER TABLE musician rename to singer;
+
+ALTER table singer drop COLUMN role;
+
+ALTER table singer drop COLUMN bandName;
+
+drop Table band;
